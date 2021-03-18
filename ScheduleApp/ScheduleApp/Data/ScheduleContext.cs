eda4e3083965a4336d97586cfe056ec7908fae41
irynashyginska <iryna.shyginska2012@gmail.com>
2021-03-18 14:12:34 +0200

    Added new files

---
+++ ScheduleApp/ScheduleApp/Data/ScheduleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ScheduleApp.Models;
+
+namespace ScheduleApp.Data
+{
+    public class ScheduleContext : DbContext
+    {
+        public ScheduleContext (DbContextOptions<ScheduleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ScheduleApp.Models.Schedule> Schedule { get; set; }
+    }
+}
